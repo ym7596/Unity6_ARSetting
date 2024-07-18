3a547c2c58e735a3f9ce8cfec6763b1f053d9c91
YONGMIN <ym7596@naver.com>
2024-07-18 12:38:29 +0900

    feat : Custom Image Library

--- Assets/Demo/Scripts/ARController.cs
+++ Assets/Demo/Scripts/ARController.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
@@ -7,84 +9,175 @@ using UnityEngine.XR.ARSubsystems;
 public class ARController : MonoBehaviour
 {
     [SerializeField] private ARTrackedImageManager _trackedImageManager;
-
-    [SerializeField] private GameObject Sinbi;
-    [SerializeField] private GameObject Sujin;
-
+    [SerializeField] private CustomImageLibrarySO _customImageLibrarySo;
     private Dictionary<string, GameObject> _cardDic;
+    private Dictionary<string, CustomData> _dicCustomCard;
+    private List<AddReferenceImageJobState> _jobStates = new List<AddReferenceImageJobState>();
+
+    private bool _isSession = false;
+
+    private void Awake()
+    {
+        if (_customImageLibrarySo)
+        {
+            _customImageLibrarySo.Init();
+            _dicCustomCard = _customImageLibrarySo.dicTrackingData;
+        }
+    }
 
     private void Start()
     {
         _cardDic = new Dictionary<string, GameObject>();
+        Init();
     }
 
     private void OnEnable()
     {
         _trackedImageManager.trackablesChanged.AddListener(OnTrackedImage);
+        
+        ARSession.stateChanged += ARSessionStateChanged;
     }
 
     private void OnDestroy()
     {
         _trackedImageManager.trackablesChanged.RemoveListener(OnTrackedImage);
+        
+        ARSession.stateChanged -= ARSessionStateChanged;
+    }
+    
+    private void ARSessionStateChanged(ARSessionStateChangedEventArgs stateEventArgs)
+    {
+        if (stateEventArgs.state is ARSessionState.SessionInitializing or ARSessionState.SessionTracking)
+            _isSession = true;
+    }
+
+    private void Init()
+    {
+        if (_dicCustomCard == null || _dicCustomCard.Count == 0)
+            return;
+        StartCoroutine(InitCo());
+    }
+
+    private IEnumerator InitCo()
+    {
+        yield return new WaitUntil(() => _isSession);
+        IReferenceImageLibrary lib = _trackedImageManager.referenceLibrary;
+
+        if (lib is MutableRuntimeReferenceImageLibrary mLib)
+        {
+            foreach (KeyValuePair<string, CustomData> data in _dicCustomCard)
+            {
+                if (data.Value.texture.isReadable == false)
+                {
+                    Debug.LogError($"Error : {data.Key} is not readable!");
+                    continue;
+                }
+
+                Texture2D readableTexture = MakeTextureReadable(data.Value.texture, TextureFormat.RGBA32);
+                
+                _jobStates.Add(mLib.ScheduleAddImageWithValidationJob(readableTexture,data.Key,_customImageLibrarySo.width));
+                yield return null;
+            }
+
+            yield return CheckJobCompletion(_customImageLibrarySo.GetImages());
+        }
+    }
+    
+    private IEnumerator CheckJobCompletion(List<Texture2D> images)
+    {
+        for (int i = 0; i < _jobStates.Count; i++)
+        {
+            var jobState = _jobStates[i];
+            var image = images[i];
+
+            while (!jobState.jobHandle.IsCompleted)
+            {
+                yield return null;
+            }
+            jobState.jobHandle.Complete();
+            if (jobState.status == AddReferenceImageJobStatus.Success)
+            {
+                Debug.Log($"Successfully added image: {image.name}");
+            }
+            else
+            {
+                Debug.LogError($"Failed to add image: {image.name} - Status: {jobState.status}");
+            }
+        }
+    }
+    
+    private Texture2D MakeTextureReadable(Texture2D originalTexture, TextureFormat format)
+    {
+        Texture2D readableTexture = new Texture2D(originalTexture.width, originalTexture.height, format, false);
+        RenderTexture rt = RenderTexture.GetTemporary(originalTexture.width, originalTexture.height);
+        Graphics.Blit(originalTexture, rt);
+        RenderTexture previous = RenderTexture.active;
+        RenderTexture.active = rt;
+        readableTexture.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
+        readableTexture.Apply();
+        RenderTexture.active = previous;
+        RenderTexture.ReleaseTemporary(rt);
+        return readableTexture;
     }
 
     private void OnTrackedImage(ARTrackablesChangedEventArgs<ARTrackedImage> eventArgs)
     {
         foreach (ARTrackedImage newImage in eventArgs.added)
         {
-            string name = newImage.referenceImage.name;
-            Vector3 newScale = new Vector3(newImage.size.x, newImage.size.y, newImage.size.x);
-            if (name == "sinbi")
-            {
-                SetInitTransform(newImage, Sinbi);
-              
-                Debug.Log("Sinbi!");
-            }
-            else if (name == "sujin")
-            {
-                SetInitTransform(newImage, Sujin);
-             
-                Debug.Log("Sujin!");
-            }
+            AddImage(newImage);
         }
 
         foreach (ARTrackedImage updatedImage in eventArgs.updated)
         {
-            string name = updatedImage.referenceImage.name;
-            if (updatedImage.trackingState == TrackingState.Tracking)
-            {
-                if (name == "sinbi")
-                {
-                    _cardDic[name].transform.localPosition = Vector3.zero;
-                    _cardDic[name].transform.localRotation =  Quaternion.Euler(new Vector3(0,180,0));
-                }
-                else if (name == "sujin")
-                {
-                    _cardDic[name].transform.localPosition = Vector3.zero;
-                    _cardDic[name].transform.localRotation =  Quaternion.Euler(new Vector3(0,180,0));
-                }
-            }   
+            UpdateImage(updatedImage);
         }
 
         foreach (var removedImage in eventArgs.removed)
         {
-           
+           RemoveImage(removedImage);
+        }
+    }
+
+    private void AddImage(ARTrackedImage trackedImage)
+    {
+        string imgname = trackedImage.referenceImage.name;
+        Debug.Log($"name : {imgname} has been reached");
+        if (_dicCustomCard.TryGetValue(imgname, out CustomData trackData))
+        {
+            Debug.Log($"AddImage: Tracking {imgname} - Prefab: {trackData.trackingPrefab}");
+            SetInitTransform(trackedImage, trackData);
         }
     }
     
+    private void UpdateImage(ARTrackedImage tracked)
+    {
+        string imgName = tracked.referenceImage.name;
 
-    private void SetInitTransform(ARTrackedImage img,GameObject go)
+        if (tracked.trackingState == TrackingState.Tracking)
+        {
+            
+        }
+    }
+    
+    private void RemoveImage(KeyValuePair<TrackableId, ARTrackedImage> tracked)
+    {
+        
+    }
+
+    private void SetInitTransform(ARTrackedImage img,CustomData go)
     {
         Vector2 imageSize = img.size;
+        if (_cardDic.Keys.Contains(img.referenceImage.name))
+            return;
         
-        GameObject o = Instantiate(go,img.transform);
+        GameObject o = Instantiate(go.trackingPrefab,img.transform);
         Vector3 newScale = new Vector3(imageSize.x, imageSize.y, imageSize.x);
         o.transform.localScale = newScale;
         o.transform.SetParent(img.transform);
         o.transform.localPosition = Vector3.zero;
-        o.transform.localRotation =  Quaternion.Euler(new Vector3(0,180,0));
+        o.transform.localRotation =  Quaternion.Euler(go.offsetRotation);
 
-
+     //  o.transform.SetParent(null);
         _cardDic[img.referenceImage.name] = o;
     }
 }
---
+++ Assets/Demo/Scripts/CustomImageLibrarySO.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CustomImageLibrarySO", menuName = "XRData/CustomImageLibrarySO", order = 1)]
+public class CustomImageLibrarySO : ScriptableObject
+{
+    public float width = 1f;
+    
+    public List<CustomData> _customDatas;
+
+    public Dictionary<string, CustomData> dicTrackingData = new Dictionary<string, CustomData>();
+
+    public void Init()
+    {
+        foreach (CustomData i in _customDatas)
+        {
+            dicTrackingData.Add(i.id,i);
+        }
+    }
+
+    public List<Texture2D> GetImages()
+    {
+        List<Texture2D> imgs = new List<Texture2D>();
+        
+        foreach (KeyValuePair<string, CustomData> data in dicTrackingData)
+            imgs.Add(data.Value.texture);
+
+        return imgs;
+    }
+}
+
+[Serializable]
+public class CustomData
+{
+    public string id;
+    public Texture2D texture;
+    public GameObject trackingPrefab;
+    public Vector3 offsetRotation;
+}
